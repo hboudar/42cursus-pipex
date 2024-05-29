52a5635879987d02ddd268f75c8e8209df492de0
Cloneg7 <151786581+Cloneg7@users.noreply.github.com>
2024-05-29 14:31:46 +0100

    Update pipex_mandatory.c

--- mandatory/pipex_mandatory.c
+++ mandatory/pipex_mandatory.c
@@ -20,6 +20,8 @@ void	error(char *msg)
 
 int	main(int ac, char *av[], char *env[])
 {
+	int	fd_in;
+	int	fd_out;
 	int	fd[2];
 	int	pid;
 
@@ -28,6 +30,7 @@ int	main(int ac, char *av[], char *env[])
 		write(2, "error: Invalid number of arguments\n", 35);
 		return (1);
 	}
+	(1) && (fd_in = dup(0), fd_out = dup(1));
 	if (pipe(fd) == -1)
 		error("pipe function");
 	pid = fork();
@@ -42,6 +45,7 @@ int	main(int ac, char *av[], char *env[])
 		check_files(dup2(fd[0], 0), 0, close(fd[0]), close(fd[1]));
 		last_process(av, env, fd);
 	}
+	(1) && (dup2(fd_in, 0), close(fd_in), dup2(fd_out, 1), close(fd_in));
 	check_env(env, 0);
 	exit (EXIT_SUCCESS);
 }
